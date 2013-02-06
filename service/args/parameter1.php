402d492f7c4e41a65109c930ce1d31859e2090c7
Haydar Kulekci <haydar.kulekci@reformo.net>
2013-02-07 01:13:54 +0200

    db example is added

--- service/args/parameter1.php
+++ service/args/parameter1.php
@@ -3,18 +3,15 @@ class parameter1 extends Controller{
 	
 	public function index($data = array()){
 
-		print_r($data);
 		echo 'parameter1 file index method is runned! Args : '.serialize($data) . PHP_EOL;
-
 		return 1;
 
 	}
 
 	public function run($data = array()){
 
-		print_r($data);
+		$this->db->query("INSERT INTO test SET test = 'test_message'");
 		echo 'parameter1 file run method is runned! Args : '.serialize($data) . PHP_EOL;
-
 		return 1;
 
 	}
