04cd75eaabb668375271b8c62ef410a5e7847f28
Haydar KÜLEKCİ <haydarkulekci@gmail.com>
2014-11-03 07:47:40 +0200

    added to route work as a service parameter

--- service/lib/route.php
+++ service/lib/route.php
@@ -1,63 +1,80 @@
 <?php
 class Route{
-	protected $registry;
-	protected $class_name;
-	protected $method_name;
-	protected $object;
-	protected $data = array();
+    protected $registry;
+    protected $class_name;
+    protected $method_name;
+    protected $object;
+    protected $data = array();
+    protected $as_a_service = false;
 
-	public function __construct($registry) {
-		$this->registry = $registry;
-	}
+    public function __construct($registry) {
+        $this->registry = $registry;
+    }
 
-	public function __get($key) {
-		return $this->registry->get($key);
-	}
+    public function __get($key) {
+        return $this->registry->get($key);
+    }
 
-	public function __set($key, $value) {
-		$this->registry->set($key, $value);
-	}
+    public function __set($key, $value) {
+        $this->registry->set($key, $value);
+    }
 
-	public function prepare($data = array()){
+    public function prepare($data = array()){
 
-		// to remove filename
-		array_shift($data);
-		if (isset( $data[0] )){
+        // to remove filename
+        array_shift($data);
 
-			$this->class_name = $data[0];
-			array_shift($data);
 
-			if (is_file( ROOT_DIR."args/" . $this->class_name . ".php" )){
+        $this->as_a_service = false;
 
-				include_once ROOT_DIR."args/" . $this->class_name . ".php";
+        if (isset($data[0]) && $data[0] == "as_a_service") {
+            print "Script will work as a service" . PHP_EOL;
+            $this->as_a_service = true;
+            array_shift($data);
+        }
 
-				if (isset( $data[0] )){
-					$this->method_name = $data[0];
-					array_shift($data);
-				}else{
-					$this->method_name = 'index';
-				}
+        if (isset( $data[0] )){
 
-				if (is_callable( array( $this->class_name, $this->method_name ) )){
+            $this->class_name = $data[0];
+            array_shift($data);
 
-					$this->object = new $this->class_name($this->registry);
-					$ret = call_user_func_array( array($this->object, $this->method_name ), array($data));
+            if (is_file( ROOT_DIR."args/" . $this->class_name . ".php" )){
 
-					return $ret;
+                include_once ROOT_DIR."args/" . $this->class_name . ".php";
 
-				}else{
+                if (isset( $data[0] )){
+                    $this->method_name = $data[0];
+                    array_shift($data);
+                }else{
+                    $this->method_name = 'index';
+                }
+                
+                if (is_callable( array( $this->class_name, $this->method_name ) )){
+                    $this->object = new $this->class_name($this->registry);
+                    $this->data = $data;
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
 
-					return 0;
+        }
+    }
 
-				}
-
-			}else{
-
-				return 0;
-
-			}
-
-		}
-	}
+    public function run()
+    {
+        if ($this->as_a_service) {
+            while (true) {
+                call_user_func_array( array($this->object, $this->method_name ), array($this->data));
+                echo PHP_EOL;
+                sleep(2);
+            }
+        } else {
+            call_user_func_array( array($this->object, $this->method_name ), array($this->data));
+            echo PHP_EOL;
+        }
+    }
 
 }
